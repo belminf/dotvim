ad0d5c29d53934689cec8a81f8e87cef05221cc5
Belmin Fernandez <belminf@gmail.com>
2024-08-05 02:00:40 -0400

    Fixes for local history, clipboard and code diagnostics

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,7 @@
+vim.o.updatetime = 250
+vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+	group = vim.api.nvim_create_augroup("float_diagnostic", { clear = true }),
+	callback = function()
+		vim.diagnostic.open_float(nil, { focus = false })
+	end,
+})
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,2 +1,9 @@
 vim.opt.mouse = ""
 vim.g.autoformat = false
+vim.opt.clipboard = ""
+
+-- paths
+vim.opt.undodir = vim.fn.stdpath("config") .. "/local/undo"
+vim.opt.backupdir = vim.fn.stdpath("config") .. "/local/backup"
+vim.opt.directory = vim.fn.stdpath("config") .. "/local/swap"
+vim.opt.viewdir = vim.fn.stdpath("config") .. "/local/views"
