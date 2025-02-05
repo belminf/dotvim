3b375440215423ae9723f77ba87c6836703fe919
Belmin Fernandez <belminf@gmail.com>
2025-02-04 19:11:09 -0500

    Re-enables grug-far

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,5 @@
 return {
 	{ "nvimdev/dashboard-nvim", enabled = false },
-	{ "MagicDuck/grug-far.nvim", enabled = false },
 	{ "folke/persistence.nvim", enabled = false },
 	{ "folke/tokyonight.nvim", enabled = false },
 }
