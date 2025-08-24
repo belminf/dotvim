b3c991cd2d1f73a93ddb9254e20c22942a1d3def
Belmin Fernandez <belminf@gmail.com>
2025-08-24 11:01:13 -0400

    Adds h key for jumping hunks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -27,6 +27,16 @@ vim.keymap.set("n", "<S-Tab><S-Tab>", "<cmd>bp<CR>", { noremap = true, silent = 
 vim.keymap.set("n", ">", vim.diagnostic.goto_next, { desc = "Focus on next diagnostic" })
 vim.keymap.set("n", "<", vim.diagnostic.goto_prev, { desc = "Focus on previous diagnostic" })
 
+vim.keymap.set("n", "h", function()
+  if vim.wo.diff then
+    -- If in diff mode, use Vim's built-in `]c` to go to the next change
+    vim.cmd.normal({ "]c", bang = true })
+  else
+    -- Otherwise, use gitsigns to go to the next hunk
+    require("gitsigns").nav_hunk("next")
+  end
+end, { desc = "Next Hunk / Diff Change" })
+
 vim.keymap.set("v", "<Tab>", ">gv", { desc = "Indent text" })
 vim.keymap.set("v", "<S-Tab>", "<gv", { desc = "Unindent text" })
 vim.keymap.set({ "n", "v" }, "F", function()
