2aa8679949311ba223d227a1798bd9173d3c9e15
Belmin Fernandez <belmin.fernandez@crowdstrike.com>
2025-09-04 22:07:52 -0400

    Fixes conform config that was overrwriting the default

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,42 +1,33 @@
 return {
 	"stevearc/conform.nvim",
-	opts = function()
-		local plugin = require("lazy.core.config").plugins["conform.nvim"]
-		---@type conform.setupOpts
-		local opts = {
-			default_format_opts = {
-				timeout_ms = 3000,
-				async = false, -- not recommended to change
-				quiet = false, -- not recommended to change
-				lsp_format = "fallback", -- not recommended to change
+	opts = function(_, opts)
+		-- Merge your custom format options
+		opts.default_format_opts = vim.tbl_deep_extend("force", opts.default_format_opts or {}, {
+			timeout_ms = 3000,
+			async = false,
+			quiet = false,
+			lsp_format = "fallback",
+		})
+
+		-- Merge your formatters_by_ft
+		opts.formatters_by_ft = vim.tbl_deep_extend("force", opts.formatters_by_ft or {}, {
+			lua = { "stylua" },
+			fish = { "fish_indent" },
+			sh = { "shfmt" },
+			go = { "goimports", "gofumpt" },
+			terraform = { "terraform_fmt" },
+			tf = { "terraform_fmt" },
+			["terraform-vars"] = { "terraform_fmt" },
+			["*"] = { "trim_whitespace", "trim_newlines" },
+		})
+
+		-- Merge your formatters
+		opts.formatters = vim.tbl_deep_extend("force", opts.formatters or {}, {
+			injected = { options = { ignore_errors = true } },
+			shfmt = {
+				prepend_args = { "-i", "2", "-ci", "-s" },
 			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				fish = { "fish_indent" },
-				sh = { "shfmt" },
-				go = { "goimports", "gofumpt" },
-				terraform = { "terraform_fmt" },
-				tf = { "terraform_fmt" },
-				["terraform-vars"] = { "terraform_fmt" },
-				["*"] = { "trim_whitespace", "trim_newlines" },
-			},
-			-- The options you set here will be merged with the builtin formatters.
-			-- You can also define any custom formatters here.
-			---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-			formatters = {
-				injected = { options = { ignore_errors = true } },
-				-- # Example of using dprint only when a dprint.json file is present
-				-- dprint = {
-				--   condition = function(ctx)
-				--     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-				--   end,
-				-- },
-				--
-				shfmt = {
-					prepend_args = { "-i", "2", "-ci", "-s" },
-				},
-			},
-		}
+		})
 		return opts
 	end,
 }
